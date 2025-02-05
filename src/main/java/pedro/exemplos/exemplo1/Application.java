aa50af24e50675f9195e5d4d563a125af45db5a2
pedro alberto <albertobolina@gmail.com>
2025-02-05 18:13:20 +0000

    novo commit

---
+++ src/main/java/pedro/exemplos/exemplo1/Application.java
@@ -0,0 +1,13 @@
+package pedro.exemplos.exemplo1;
+
+public class Application {
+    
+    public Application(){ 
+        System.out.println("Inside Application");
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Starting Application");
+        Application app = new Application();
+    }
+}
