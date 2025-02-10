da5f2928671aae8831ea35cf6d2ac8f38dc0bf75
pedro alberto <albertobolina@gmail.com>
2025-02-10 17:56:35 +0000

    novo connit

--- src/main/java/pedro/exemplos/exemplo1/Application.java
+++ src/main/java/pedro/exemplos/exemplo1/Application.java
@@ -1,7 +1,21 @@
 package pedro.exemplos.exemplo1;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
     
+    public void greet(){
+
+        List<String> greetings = new ArrayList<>();
+        greetings.add("Olá, como estás");
+        greetings.add("Bom dia alegria");
+
+        for (String greeting : greetings) {
+            System.out.println("Greeting : " + greeting);
+        }
+    }
+
     public Application(){ 
         System.out.println("Inside Application");
     }
@@ -9,5 +23,6 @@ public class Application {
     public static void main(String[] args) {
         System.out.println("Starting Application");
         Application app = new Application();
+        app.greet();
     }
 }
